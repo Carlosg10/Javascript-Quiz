585e8789bdde97ad1147458ce0296e53de631a6e
Carlos Gonzales <gonzalescarlos638@gmail.com>
2018-10-25 21:19:25 -0400

    1st commit

---
+++ final-project.js
@@ -0,0 +1,25 @@
+var score = 0
+$("#btn").on('click', function(){
+  if($("#q1c").is(":checked")){
+    score++
+  }
+  if($("#q2a").is(":checked")){
+    score++
+    alert(score)
+  }
+  if($("#q3a").is(":checked")){
+    score++
+    alert(score)
+  }
+  if("#q4b").is(";checked"){
+    score++
+    alert(score)
+  }
+  if("#5qa").is(";checked"){
+    score++
+    alert(score)
+  }
+  if("#6a").is(":checked")
+  score++
+  alert(score)
+})
