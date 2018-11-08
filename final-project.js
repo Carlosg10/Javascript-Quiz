060d1e8fd03ed5d61a85a64ef93c30ddbb40fa24
Carlos Gonzales <gonzalescarlos638@gmail.com>
2018-11-07 21:54:09 -0500

    3rd commit

--- final-project.js
+++ final-project.js
@@ -1,71 +1,75 @@
 var score = 0
-$("#btn").on('click', function(){
-  if($("#q1c").is(":checked")){
+$("#btn").on('click', function() {
+  if ($("#q1c").is(":checked")) {
     score++
   }
-  if($("#q2a").is(":checked")){
+  if ($("#q2a").is(":checked")) {
     score++
-    alert(score)
+
   }
-  if($("#q3a").is(":checked")){
+  if ($("#q3a").is(":checked")) {
     score++
-    alert(score)
+
   }
-  if($("#q4b").is(":checked")){
+  if ($("#q4b").is(":checked")) {
     score++
-    alert(score)
+
   }
-  if($("#5qa").is(":checked")){
+  if ($("#5qa").is(":checked")) {
     score++
-    alert(score)
+
   }
-  if($("#6a").is(":checked")){
+  if ($("#6a").is(":checked")) {
     score++
-    alert(score)
+
   }
-if($("#7c").is(":checked")){
-  score++
-  alert(score)
-}
-if($("#8a").is(":checked")){
-  score++
-  alert(score)
-}
-if($("#9a").is(":checked")){
-  score++
-  alert(score)
-}
-if($("#10submit").is(":checked")){
-  score++
-  alert(score)
-}
+  if ($("#7c").is(":checked")) {
+    score++
+
+  }
+  if ($("#8a").is(":checked")) {
+    score++
+
+  }
+  if ($("#9a").is(":checked")) {
+    score++
+
+  }
+  if ($("#10submit").is(":checked")) {
+    score++
+
+  }
 })
 
-$("#next2").on('click', function(){
+$("#next2").on('click', function() {
   $("#question2").css("display", "block")
 })
 
-$("#next3").on('click', function(){
+$("#next3").on('click', function() {
   $("#question3").css("display", "block")
 })
-$("#next4").on('click', function(){
+$("#next4").on('click', function() {
   $("#question4").css("display", "block")
 })
-$("#next5").on('click', function(){
+$("#next5").on('click', function() {
   $("#question5").css("display", "block")
 })
-$("#next6").on('click', function(){
+$("#next6").on('click', function() {
   $("#question6").css("display", "block")
 })
-$("#next7").on('click', function(){
+$("#next7").on('click', function() {
   $("#question7").css("display", "block")
 })
-$("#next8").on('click', function(){
+$("#next8").on('click', function() {
   $("#question8").css("display", "block")
 })
-$("#next9").on('click', function(){
+$("#next9").on('click', function() {
   $("#question9").css("display", "block")
 })
-$("#next10").on('click', function(){
+$("#next10").on('click', function() {
   $("#question10").css("display", "block")
+})
+
+$("#btn").on('click', function() {
+  $("#result").html(score)
 })
